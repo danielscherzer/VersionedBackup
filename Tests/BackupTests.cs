bcb596e5b1be74fa95cc6a4631bccf6263d2262b
Daniel Scherzer <scherzer@rwu.de>
2022-01-17 09:36:07 +0100

    quick commit

--- Tests/BackupTests.cs
+++ Tests/BackupTests.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using VersionedBackup.Interfaces;
-using VersionedBackup.Services;
 using VersionedBackupTests.Services;
 
 namespace VersionedBackup.Tests
@@ -47,32 +47,61 @@ namespace VersionedBackup.Tests
 		[TestMethod()]
 		public void RunCopyFilesTest()
 		{
-			//for(int i = 0; i < 10000; ++i)
-			{
-				var fileSystem = new VirtualFileSystem();
-				fileSystem.CreateDirectory(dirs.SourceDirectory);
-				var srcFiles = new string[] { "a", "b", "c" };
-				foreach (var file in srcFiles) fileSystem.CreateFile(Path.Combine(dirs.SourceDirectory, file));
-				Backup.Run(new TestOptions(dirs), nullReport, fileSystem, token);
-				foreach (var file in srcFiles) Assert.IsTrue(fileSystem.ExistsFile(Path.Combine(dirs.DestinationDirectory, file)));
-			}
+			var fileSystem = new VirtualFileSystem();
+			fileSystem.CreateDirectory(dirs.SourceDirectory);
+			var srcFiles = new string[] { "d", "b", "c", "z" };
+			foreach (var file in srcFiles) fileSystem.CreateFile(Path.Combine(dirs.SourceDirectory, file));
+			Backup.Run(new TestOptions(dirs), nullReport, fileSystem, token);
+			foreach (var file in srcFiles) Assert.IsTrue(fileSystem.ExistsFile(Path.Combine(dirs.DestinationDirectory, file)));
 		}
 
 		[TestMethod()]
 		public void RunCopyTest()
 		{
-			//for (int i = 0; i < 100; ++i)
+			var fileSystem = new VirtualFileSystem();
+			fileSystem.CreateDirectory(dirs.SourceDirectory);
+			var srcDirs = new string[] { "a", dirs.SourceDirectory + "a", "b", "c", "d" };
+			foreach (var subDir in srcDirs) fileSystem.CreateDirectory(Path.Combine(dirs.SourceDirectory, subDir));
+			var srcFiles = new string[] { "x", "y", "z" };
+			foreach (var file in srcFiles) fileSystem.CreateFile(Path.Combine(dirs.SourceDirectory, srcDirs[0], file));
+			Backup.Run(new TestOptions(dirs), nullReport, fileSystem, token);
+			foreach (var file in srcFiles) Assert.IsTrue(fileSystem.ExistsFile(Path.Combine(dirs.DestinationDirectory, srcDirs[0], file)));
+			foreach (var subDir in srcDirs) Assert.IsTrue(fileSystem.ExistsDirectory(Path.Combine(dirs.DestinationDirectory, subDir)));
+		}
+
+		[TestMethod()]
+		public void RunBigCopyTest()
+		{
+			var fileSystem = new VirtualFileSystem();
+			List<string> srcFiles = new();
+			List<string> srcSubDirs = new();
+			Random rnd = new(21);
+			int relativeStart = dirs.SourceDirectory.Length + 1;
+			void FillFileSystem(string dir)
 			{
-				var fileSystem = new VirtualFileSystem();
-				fileSystem.CreateDirectory(dirs.SourceDirectory);
-				var srcDirs = new string[] { "a", dirs.SourceDirectory + "a", "b", "_" };
-				foreach (var subDir in srcDirs) fileSystem.CreateDirectory(Path.Combine(dirs.SourceDirectory, subDir));
-				var srcFiles = new string[] { "x", "y", "z" };
-				foreach (var file in srcFiles) fileSystem.CreateFile(Path.Combine(dirs.SourceDirectory, srcDirs[0], file));
-				Backup.Run(new TestOptions(dirs), nullReport, fileSystem, token);
-				foreach (var file in srcFiles) Assert.IsTrue(fileSystem.ExistsFile(Path.Combine(dirs.DestinationDirectory, srcDirs[0], file)));
-				foreach (var subDir in srcDirs) Assert.IsTrue(fileSystem.ExistsDirectory(Path.Combine(dirs.DestinationDirectory, subDir)));
+				// subdirs
+				for (int i = rnd.Next(0, 10); i > 0; --i)
+				{
+					char c = (char)((int)'A' + i);
+					string path = Path.Combine(dir, c.ToString());
+					fileSystem.CreateDirectory(path);
+					srcSubDirs.Add(path[relativeStart..]);
+					if (0 == rnd.Next(10)) FillFileSystem(path); // recursion
+				}
+				// files
+				for (int i = rnd.Next(0, 30); i > 0; --i)
+				{
+					string path = Path.Combine(dir, i.ToString());
+					fileSystem.CreateFile(path);
+					srcFiles.Add(path[relativeStart..]);
+				}
 			}
+			fileSystem.CreateDirectory(dirs.SourceDirectory);
+			FillFileSystem(dirs.SourceDirectory);
+
+			Backup.Run(new TestOptions(dirs), nullReport, fileSystem, token);
+			foreach (var file in srcFiles) Assert.IsTrue(fileSystem.ExistsFile(Path.Combine(dirs.DestinationDirectory, file)));
+			foreach (var subDir in srcSubDirs) Assert.IsTrue(fileSystem.ExistsDirectory(Path.Combine(dirs.DestinationDirectory, subDir)));
 		}
 	}
 }
--- Tests/Services/VirtualFileSystem.cs
+++ Tests/Services/VirtualFileSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using VersionedBackup.Interfaces;
 using VersionedBackup.PathHelper;
@@ -7,25 +8,25 @@ namespace VersionedBackupTests.Services
 {
 	internal class VirtualFileSystem : IFileSystem
 	{
-		public bool Copy(string srcFilePath, string dstFilePath) => files.Add(dstFilePath);
+		public bool Copy(string srcFilePath, string dstFilePath) => files.TryAdd(dstFilePath, 0);
 
-		public bool CreateDirectory(string name) => dirs.Add(NormalizeDir(name));
+		public bool CreateDirectory(string name) => dirs.TryAdd(NormalizeDir(name), 0);
 
 		public IEnumerable<string> EnumerateDirsRecursive(string dir)
 		{
 			dir = NormalizeDir(dir);
-			return dirs.Where(subDir => subDir.StartsWith(dir)).Select(subDir => subDir);
+			return dirs.Keys.Where(subDir => subDir.StartsWith(dir)).Select(subDir => subDir);
 		}
 
 		public IEnumerable<string> EnumerateFiles(IEnumerable<string> dirs) =>
 			from dir in dirs.AsParallel()
-			from file in files
+			from file in files.Keys
 			where file.StartsWith(dir)
 			select file;
 
-		public bool ExistsDirectory(string name) => dirs.Contains(NormalizeDir(name));
+		public bool ExistsDirectory(string name) => dirs.ContainsKey(NormalizeDir(name));
 
-		public bool ExistsFile(string name) => files.Contains(name);
+		public bool ExistsFile(string name) => files.ContainsKey(name);
 
 		public bool HasChanged(string srcFilePath, string dstFilePath)
 		{
@@ -34,18 +35,18 @@ namespace VersionedBackupTests.Services
 
 		public bool MoveDirectory(string source, string destination)
 		{
-			return dirs.Remove(source) && dirs.Add(destination);
+			return dirs.Keys.Remove(source) && dirs.TryAdd(destination, 0);
 		}
 
 		public bool MoveFile(string source, string destination)
 		{
-			return files.Remove(source) && files.Add(destination);
+			return files.Keys.Remove(source) && files.TryAdd(destination, 0);
 		}
 
-		internal void CreateFile(string name) => files.Add(name);
+		internal void CreateFile(string name) => files.TryAdd(name, 0);
 
-		private readonly HashSet<string> dirs = new();
-		private readonly HashSet<string> files = new();
+		private readonly ConcurrentDictionary<string,int> dirs = new();
+		private readonly ConcurrentDictionary<string, int> files = new();
 
 		private static string NormalizeDir(string dir) => dir.IncludeTrailingPathDelimiter();
 		//private static string NormalizeDir(string dir) => Path.TrimEndingDirectorySeparator(dir); // will not work because of make relative
