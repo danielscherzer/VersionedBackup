fef9c429c3e7c639c38ab9ab969cb9bbffc346b6
Daniel Scherzer <scherzer@rwu.de>
2022-03-13 11:12:10 +0100

    quick commit

---
+++ Tests/ParseTests.cs
@@ -0,0 +1,74 @@
+using CommandLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using VersionedCopy.Interfaces;
+using VersionedCopy.Options;
+using VersionedCopy.PathHelper;
+
+namespace VersionedCopy.Tests
+{
+	[TestClass()]
+	public class ParseTests
+	{
+		static bool IsReadOnly(string arg)
+		{
+			arg = arg.ToLowerInvariant();
+			return arg.StartsWith("--read") || arg.StartsWith("-r");
+		}
+
+		static string Dir(string dir) => dir.IncludeTrailingPathDelimiter();
+
+		static IEnumerable<string> TakeOption(IEnumerable<string> seq, string start)
+		{
+			return seq.SkipWhile(arg => !arg.ToLowerInvariant().StartsWith(start.ToLowerInvariant()))
+				.Skip(1).TakeWhile(arg => !arg.StartsWith("-"));
+		}
+
+		[DataTestMethod()]
+		[DataRow("mirror", "c:\\src", "d:\\dst")]
+		[DataRow("update", "c:\\src", "d:\\dst")]
+		[DataRow("sync", "c:\\src", "d:\\dst")]
+		[DataRow("sync", "d:\\daten", "e:\\daten", "--Readonly")]
+		[DataRow("sync", "d:\\daten", "e:\\daten", "--ignoreDirectories", "TestResults")]
+		[DataRow("synC", "d:\\daten", "e:\\daten", "--ignoreDirectories", ".vs", "bin", "obj", "TestResults")]
+		[DataRow("sync", "d:\\daten", "e:\\daten", "--ignoreFiles", "desktop.ini")]
+		[DataRow("sync", "d:\\daten", "e:\\daten", "--ignoreFiles", "desktop.ini", @"Visual Studio 2022\Visualizers\attribcache140.bin")]
+		[DataRow("sync", "d:\\daten", "e:\\daten", "--readOnly", "--ignoreDirectories", ".vs", "bin", "obj", "TestResults", "--ignoreFiles", "desktop.ini", @"Visual Studio 2022\Visualizers\attribcache140.bin")]
+		[DataRow("Sync", "d:\\daten", "e:\\daten", "--ignoreFiles", "desktop.ini", "--ignoreDirectories", ".vs")]
+		public void ParseArgumentsTest(params string[] args)
+		{
+			void AllTest(IOptions options)
+			{
+				Assert.AreEqual(Dir(args[1]), options.SourceDirectory);
+				Assert.AreEqual(Dir(args[2]), options.DestinationDirectory);
+				Assert.AreEqual(args.Any(arg => IsReadOnly(arg)), options.ReadOnly);
+
+				var ignoreDirs = TakeOption(args, "--ignoreD").Select(dir => Dir(dir)).ToArray();
+				if (ignoreDirs.Any())
+				{
+					CollectionAssert.AreEquivalent(ignoreDirs, options.IgnoreDirectories.ToArray());
+				}
+				var ignoreFiles = TakeOption(args, "--ignoreF").ToArray();
+				if (ignoreFiles.Any())
+				{
+					CollectionAssert.AreEquivalent(ignoreFiles, options.IgnoreFiles.ToArray());
+				}
+			}
+			void Verb(string verb, IOptions options)
+			{
+				Assert.AreEqual(args[0].ToLowerInvariant(), verb);
+				AllTest(options);
+			}
+			void Error(IEnumerable<Error> errors)
+			{
+				Assert.Fail(string.Join('\n', errors.Select(error => error.ToString())));
+			}
+			Program.Parse(args)
+				.WithParsed<MirrorOptions>(opt => Verb("mirror", opt))
+				.WithParsed<UpdateOptions>(opt => Verb("update", opt))
+				.WithParsed<SyncOptions>(opt => Verb("sync", opt))
+				.WithNotParsed(Error);
+		}
+	}
+}
--- src/Program.cs
+++ src/Program.cs
@@ -10,6 +10,7 @@ namespace VersionedCopy
 {
 	public static class Program
 	{
+		
 		public static void Main(string[] args)
 		{
 #if DEBUG
@@ -25,8 +26,7 @@ namespace VersionedCopy
 				args.Cancel = true; // means to continue the process!, no hard cancel, but give process time to cleanup
 			};
 
-			var parser = new Parser(with => { with.CaseSensitive = false; with.AutoHelp = true; with.HelpWriter = Console.Error; });
-			var result = parser.ParseArguments<MirrorOptions, UpdateOptions, SyncOptions, SnapshotOptions, AssemblyUpdateOptions>(args)
+			Parse(args)
 				.WithParsed<MirrorOptions>(options => Mirror.Run(new AlgorithmEnv(options, output, cts.Token)))
 				.WithParsed<UpdateOptions>(options => Update.Run(new AlgorithmEnv(options, output, cts.Token)))
 				.WithParsed<SyncOptions>(options => Sync.Run(new AlgorithmEnv(options, output, cts.Token)))
@@ -35,6 +35,13 @@ namespace VersionedCopy
 #if DEBUG
 			stopwatch.Benchmark("Total");
 #endif
+		}
+
+		public static ParserResult<object> Parse(string[] args)
+		{
+			var parser = new Parser(with => { with.CaseSensitive = false; with.AutoHelp = true; with.HelpWriter = Console.Error; });
+			var result = parser.ParseArguments<MirrorOptions, UpdateOptions, SyncOptions, SnapshotOptions, AssemblyUpdateOptions>(args);
+			return result;
 		}
 	}
 }
