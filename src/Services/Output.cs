4ba127624e4ced6989e06444105d94fc8827c4d6
Daniel Scherzer <scherzer@rwu.de>
2022-05-29 07:11:11 +0200

    quick commit

--- src/Services/Output.cs
+++ src/Services/Output.cs
@@ -1,18 +1,38 @@
 using System;
+using System.IO;
 using VersionedCopy.Interfaces;
 
 namespace VersionedCopy.Services
 {
 	public class Output : IOutput
 	{
+		private string logFile = string.Empty;
+
 		public void Error(string message)
 		{
 			Console.Error.WriteLine($"ERROR: {message}");
+			Log($"ERROR: {message}");
+		}
+
+		public void SetLogFile(string path)
+		{
+			logFile = path;
+			var dir = Path.GetDirectoryName(path);
+			if(dir is not null) Directory.CreateDirectory(dir);
 		}
 
 		public void Report(string message)
 		{
 			Console.WriteLine(message);
+			Log(message);
+		}
+
+		private void Log(string message)
+		{
+			if (!string.IsNullOrEmpty(logFile))
+			{
+				File.AppendAllText(logFile, $"{DateTime.Now:yyyy-MM-dd_HHmmss} : {message}");
+			}
 		}
 	}
 }
--- src/Services/SrcDstEnv.cs
+++ src/Services/SrcDstEnv.cs
@@ -1,14 +1,17 @@
+using System.IO;
 using System.Threading;
 using VersionedCopy.Interfaces;
+using VersionedCopy.PathHelper;
 
 namespace VersionedCopy.Services
 {
 	public class SrcDstEnv : Env
 	{
-		public SrcDstEnv(ISrcDstOptions options, IOutput output, CancellationToken token) : base(options, output, token)
+		public SrcDstEnv(ISrcDstOptions options, Output output, CancellationToken token) : base(options, output, token)
 		{
 			Options = options;
 			EnvExtensions.Setup(options.SourceDirectory, options.DestinationDirectory, output, options.ReadOnly, FileSystem);
+			output.SetLogFile(Path.Combine(Snapshot.GetMetaDataDir(options.SourceDirectory), "operations.log"));
 		}
 
 		public ISrcDstOptions Options { get; }
